9c3d45f8d66d9c78915f2ae9ed15278232ccc4ad
Daniel Hu <1004317985@qq.com>
2021-06-05 15:11:38 +0800

    feat: 修改lint规则

--- temp/react-ts/.eslintrc.js
+++ temp/react-ts/.eslintrc.js
@@ -27,6 +27,7 @@ module.exports = {
     '@typescript-eslint/ban-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/ban-types': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
     'react-hooks/rules-of-hooks': 'error', // 检查 Hook 的规则
     'react-hooks/exhaustive-deps': 'warn' // 检查 effect 的依赖
   }
--- temp/vue-ts/.eslintrc.js
+++ temp/vue-ts/.eslintrc.js
@@ -15,6 +15,7 @@ module.exports = {
   rules: {
     '@typescript-eslint/ban-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
     'space-before-function-paren': 'off'
   }
 }
